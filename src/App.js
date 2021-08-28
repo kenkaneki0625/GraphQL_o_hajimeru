6ccbc9a4667632a52979be2dda720c8f42a440b5
Shaon Baidya <kenkaneki0625@gmail.com>
2021-08-28 23:53:14 +0530

    apollo graphql package setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from "apollo-client";
+import {ApolloProvider}  from "@apollo/react-hooks";
+
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.sh'
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>Pokedex here!</p>
   );
 }
 
