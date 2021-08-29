fa146a830e25564c6cc03b9ce4762b06df2d5b8c
Shaon Baidya <kenkaneki0625@gmail.com>
2021-08-29 15:38:19 +0530

    integration with graphql and UI upgrade

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import ApolloClient from "apollo-client";
+import React from 'react';
+import ApolloClient from "apollo-boost";
 import {ApolloProvider}  from "@apollo/react-hooks";
-
+import {PokemonsContainer} from './containers/PokemonContainer';
 
 function App() {
   const client = new ApolloClient({
-    uri: 'https://graphql-pokemon.sh'
+    uri: 'https://graphql-pokemon2.vercel.app/'
+    //https://graphql-pokemon.now.sh/
   });
 
   return (
-    <p>Pokedex here!</p>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer/>
+      </main>
+    </ApolloProvider>
   );
 }
 
